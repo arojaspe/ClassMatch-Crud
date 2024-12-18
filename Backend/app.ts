bca2d1a64a5f7975ff33fe5030a18a1b35b26864
arojaspe <arojaspe@unal.edu.co>
2024-12-17 20:07:25 -0500

    chore: :wrench: add CORS support to `app.ts`

--- Backend/app.ts
+++ Backend/app.ts
@@ -1,7 +1,21 @@
-import dotenv from "dotenv"; 
-import Server from  "./Server";
+import dotenv from "dotenv";
+import Server from "./Server";
+import Cors from "cors";
+import express from "express";
 
 dotenv.config();
 
-const serv= new Server();
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:",
+};
+
+app.use(Cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+const serv = new Server();
 serv.listen();
