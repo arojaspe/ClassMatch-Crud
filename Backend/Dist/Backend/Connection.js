0b1c6a18d9ab0bac500d9db548202bcf1a9df459
7BatStrokes <daviseven84@gmail.com>
2024-12-19 02:01:49 -0500

    Test v2

--- Backend/Dist/Backend/Connection.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize("lab0_crud", "admin", "00000000", {
-    host: "db-lab0.ct6wcoa8uyye.us-east-2.rds.amazonaws.com",
-    dialect: "mysql",
-});
-exports.default = db;
