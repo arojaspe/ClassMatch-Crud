e6599ea01e5cd18517f763ca42af6d5e13e2f7dd
7BatStrokes <daviseven84@gmail.com>
2024-12-19 04:21:11 -0500

    Text v2.6

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "ClassMatch-Crud/tree/Deployment",
+  base: './',
   plugins: [react()],
   server: {
     host: "0.0.0.0",
